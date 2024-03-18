040378dafcc6111d187c4058bf38c17506cd25d1
carlosgarita <93843984+carlosgarita@users.noreply.github.com>
2024-03-18 02:15:19 +0000

    Add files via upload

---
+++ Gestor de Proveedores/src/gestor/de/proveedores/UnderflowException.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestor.de.proveedores;
+
+/**
+ *
+ * @author HP
+ */
+public class UnderflowException extends RuntimeException {
+    public UnderflowException(String message) {
+        super(message);
+    }
+}
